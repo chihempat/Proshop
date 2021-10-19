7cde6295994925fb51eeb3631fbe03129dab6cf4
Chintan <chihempat@gmail.com>
2021-10-19 17:57:35 +0530

    Feat: added BrowserRouter

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,19 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
 import Rating from './Rating';
 
 const Product = ({product}) => {
   return (
     <Card className='my-3 p-3 rounded'>
-      <a href={`/product/${product._id}`}>
+      <Link to={`/product/${product._id}`}>
         <CardImg src={product.image} variant='top'/>
-      </a>
+      </Link>
 
       <CardBody>
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <CardTitle as='div'><strong>{product.name}</strong></CardTitle>
-        </a>
-        <CardText as='div'><Rating value={product.rating} text={`${product.numReviews} reviews`} /></CardText>
+        </Link>
+        <CardText as='div'>
+          <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+        </CardText>
         <CardText as='h3'>${product.price}</CardText>
       </CardBody>
     </Card>
