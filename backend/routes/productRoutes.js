db6fa1ef49817c3bd78245bc4e0728650a348ab4
Chintan <chihempat@gmail.com>
2021-10-20 00:32:39 +0530

    Feat: added error handler

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -21,7 +21,8 @@ router.get('/:id',asyncHandler(async(req, res) => {
   const product = await Product.findById(req.params.id);
 
   if (!product) {
-    res.status(404).json({ message: 'Product not found' });
+    res.status(404);
+    throw new Error('Product not found');
   } else {
     res.json(product);
   }
