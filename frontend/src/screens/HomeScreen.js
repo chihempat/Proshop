4947162ee69f189d89c62d610d6feec52a56241e
Chintan <chihempat@gmail.com>
2021-10-20 01:55:30 +0530

    Added: Loader

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,7 +3,9 @@ import {useDispatch,useSelector} from 'react-redux';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
 import axios from 'axios';
-import {listProducts} from '../actions/productActions';
+import { listProducts } from '../actions/productActions';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 
 
 
@@ -24,7 +26,7 @@ const HomeScreen = () => {
   return (
     <>
       <h1>Latest Products</h1>
-      {loading ? <h2>Loading...</h2> : error ? <h2>{error}</h2> :
+      {loading ? <Loader />: error ? <Message variant='danger'>{error}</Message> :
         <Row>
           {products.map(product => (
             <Col key={product._id} xs={15} sm={12} md={6} lg={4} xl={3}>
