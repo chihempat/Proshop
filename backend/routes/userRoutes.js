040bcdba711f6e807779fedb531f156f83c4be40
Chintan <chihempat@gmail.com>
2021-10-21 17:03:10 +0530

    fix: added be

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,16 +1,34 @@
 import express from 'express';
 const router = express.Router();
-import { authUser, getUserProfile, registerUser } from '../controllers/userController.js';
-import { protect } from '../middleware/authMiddleware.js';
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
+} from '../controllers/userController.js';
+import { protect,admin } from '../middleware/authMiddleware.js';
 
 
 router.route('/')
-  .post(registerUser);
+  .post(registerUser)
+  .get(protect, admin, getUsers);
 
 router.post('/login', authUser);
+
 router
   .route('/profile')
-  .get(protect, getUserProfile);
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
+
+router
+  .route('/:id')
+  .get(protect,admin, getUserById)
+  .put(protect, admin, updateUser)
+  .delete(protect, admin, deleteUser);
 
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 
 import express from 'express';
+import path from 'path';
 import dotenv from 'dotenv'
 import colors from 'colors';
+import morgan from 'morgan';
 import connectDB from './config/db.js';
 import {notFound, errorHandler} from './middleware/errorMiddleware.js';
 
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
+
 dotenv.config();
 
 connectDB();
@@ -17,11 +22,29 @@ app.use(express.json()); //Used to parse JSON bodies
 app.use(express.urlencoded({ extended: true })) //Used to parse URL encoded bodies
 
 app.use('/api/products', productRoutes);
-app.use('/api/users', userRoutes)
+app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
+
+
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+app.get('/api/config/paypal', (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+)
+
+
+app.get('/', (req, res) => {
+  res.send('API is running....')
+})
+
 
 
 app.use(notFound);
 app.use(errorHandler);
+
 
 
 const PORT = process.env.PORT || 5000;
